8a5602d61ea28c5cedf5f85b9f0dc74ed06c1eed
João Paulo Lima Ferreira <joaopaulolfdev@gmail.com>
2023-09-15 16:05:16 -0300

    Start no project

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from '../pages/Home.module.css'
+
+export default function Home() {
+    return (
+        <container className={styles.home_container}>
+            <h1 className={styles.home_loader}>Abra sua conta e aposte no melhor <b>bolão do cariri!</b></h1>
+        </container>
+
+    )
+}
+
